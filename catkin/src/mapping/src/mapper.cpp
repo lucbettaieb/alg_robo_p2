aac3debcd2368c3f8cfed2773f778f51c96f0a6b
Luc Bettaieb <luc.bettaieb@gmail.com>
2014-12-12 20:56:45 -0500

    Working more on planner.  Need to weigh different options of either creating an extensive message system for passing a Quadtree through ROS msgs or populating the tree in the global planner and passing the 2d occupancy grid map through the ROS message systsm instead.  Probably should go with the latter.

--- catkin/src/mapping/src/mapper.cpp
+++ catkin/src/mapping/src/mapper.cpp
@@ -15,8 +15,55 @@
 
 #include <tf/transform_listener.h>
 #include <costmap_2d/costmap_2d_ros.h>
+#include <nav_msgs/OccupancyGrid.h>
 
+#include <vector>
 
+bool gotGrid = false;
+ros::Subscriber sub_map_;
+
+std::vector< std::vector<int> > map_; //global map to better represent occupancy grid map
+
+class QNode {
+	QNode *nw, *ne, *sw, *se; //Pointers to children nodes of the individual QNode
+	bool occupied;
+
+	public:
+		QNode getNW();
+		QNode getNE();
+		QNode getSW();
+		QNode getSE();
+};
+
+class Quadtree {
+	QNode root;
+	public:
+		Quadtree();
+		void grow(bool nwOcc, bool neOcc, bool swOcc, bool seOcc);
+};
+
+void convertOccupancyGridTo2DVector(nav_msgs::OccupancyGrid grid, std::vector< std::vector<int> > &map){		
+	int row = 0;
+	int col = 0;
+	
+	for(int j = 0; j < grid.info.height; j++, col++){
+		if(j % grid.info.width == 0) {
+			row++;
+			col = 0;
+		}
+		map.at(row).at(col) = grid.data.at(j);
+		//map[row, col] = grid[j] 
+		//Thanks sds
+	}
+}
+
+void gotMapCB(const nav_msgs::OccupancyGrid &grid){
+	if(!gotGrid){ //If a grid has not already been created...  No need to make another as the environment is static.
+		convertOccupancyGridTo2DVector(grid, map_);
+	}
+	
+	gotGrid = true; //Should have finished nicely!
+}
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "mapper");
@@ -25,9 +72,13 @@ int main(int argc, char** argv){
 	tf::TransformListener tf_(ros::Duration(10));
 	costmap_2d::Costmap2DROS costmap_("costmap", tf_);
 
+	/*These should be handled in a launch file */
 	//nh.setParam("costmap/global_frame", "/map"); //Default characteristics.
-	nh.setParam("costmap/robot_base_frame", "/robot0");
-	nh.setParam("costmap/rolling_window", true);
+	//nh.setParam("costmap/robot_base_frame", "/robot0");
+	//nh.setParam("costmap/rolling_window", true);
+
+	sub_map_ = nh.subscribe("/map", 10, gotMapCB);
+
 
 
 	ros::spin();
---
+++ catkin/src/planning/src/global_planner.cpp
@@ -0,0 +1,12 @@
+/*
+* global_planner
+* A node that will generate a global path through a quadtree from a pose to a destination.
+*
+*	Luc A. Bettaieb
+* 	bettaieb@case.edu
+*
+*	12.12.14
+*/
+#include <ros/ros.h>
+
+#include <vector>
